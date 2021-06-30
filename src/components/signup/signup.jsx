43fceac9be11c3fd766df6cbbc009be3f3dc2cea
Soojin <soojl1005@naver.com>
2021-06-30 23:15:05 +0900

    CSS: 회원가입 페이지

--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import styles from "./signup.module.css";
+import Img1 from "../../image/fruit_dish_shadow.png";
 
 const SignUp = ({ authService }) => {
   const [email, setEmail] = useState("");
@@ -38,64 +39,67 @@ const SignUp = ({ authService }) => {
   };
 
   return (
-    <>
-      <div className={styles.signup}>
-        <h1 className={styles.title}>회원가입</h1>
-        <form className={styles.singup_form} onSubmit={handleSubmit}>
-          <div className={styles.email}>
-            <p className={styles.text_email}>Email</p>
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요"
-              name="email"
-              value={email}
-              onChange={handleChange}
-              className={styles.input_email}
-            />
-          </div>
-          <div className={styles.password}>
-            <p className={styles.text_password}>Password</p>
-            <input
-              type="password"
-              placeholder="비밀번호를 8자리 이상 입력하세요"
-              name="password"
-              value={password}
-              onChange={handleChange}
-              className={styles.input_password}
-            />
-          </div>
-          <div className={styles.name}>
-            <p className={styles.text_name}>Name</p>
-            <input
-              type="name"
-              placeholder="이름을 입력하세요"
-              name="username"
-              value={username}
-              onChange={handleChange}
-              className={styles.input_name}
-            />
-          </div>
-          <div className={styles.favoriteFood}>
-            <p className={styles.text_food}>Favorite Food</p>
-            <input
-              type="food"
-              placeholder="좋아하는 음식 하나를 입력하세요"
-              name="food"
-              value={food}
-              onChange={handleChange}
-              className={styles.input_food}
-            />
-          </div>
-        </form>
-        <button
-          className={styles.BtnSignUp}
-          type="submit"
-          onClick={handleSubmit}
-        >
-          가입하기
-        </button>
+    <div className={styles.SignUp}>
+      <div className={styles.signup_container}>
+        <section className={styles.Image}></section>
+        <section className={styles.signup}>
+          <h1 className={styles.title}>회원가입</h1>
+          <form className={styles.singup_form} onSubmit={handleSubmit}>
+            <div className={styles.email}>
+              <p className={styles.text_email}>Email</p>
+              <input
+                type="email"
+                placeholder="이메일을 입력하세요"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                className={styles.input_email}
+              />
+            </div>
+            <div className={styles.password}>
+              <p className={styles.text_password}>Password</p>
+              <input
+                type="password"
+                placeholder="비밀번호를 8자리 이상 입력하세요"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                className={styles.input_password}
+              />
+            </div>
+            <div className={styles.name}>
+              <p className={styles.text_name}>Name</p>
+              <input
+                type="name"
+                placeholder="이름을 입력하세요"
+                name="username"
+                value={username}
+                onChange={handleChange}
+                className={styles.input_name}
+              />
+            </div>
+            <div className={styles.favoriteFood}>
+              <p className={styles.text_food}>Favorite Food</p>
+              <input
+                type="food"
+                placeholder="좋아하는 음식 하나를 입력하세요"
+                name="food"
+                value={food}
+                onChange={handleChange}
+                className={styles.input_food}
+              />
+            </div>
+          </form>
+          <button
+            className={styles.BtnSignUp}
+            type="submit"
+            onClick={handleSubmit}
+          >
+            Sign Up !
+          </button>
+        </section>
       </div>
-    </>
+    </div>
   );
 };
 
